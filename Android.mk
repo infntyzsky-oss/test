050453a090e3523a22961d4296828242c9e0be7c
infntyzsky-oss <infntyzsky@gmail.com>
2026-01-01 10:14:14 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,11 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := reshade
+LOCAL_MODULE    := reshade
 LOCAL_SRC_FILES := reshade.cpp
-LOCAL_CPP_EXTENSION := .cpp
-
-LOCAL_CFLAGS += -O3 -fvisibility=hidden -w
-LOCAL_LDLIBS := -llog -lEGL -lGLESv3 -ldl
+LOCAL_LDLIBS    := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)

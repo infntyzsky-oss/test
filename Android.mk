24f6d6a7512884f94685d3c45e6109be6c16f566
infntyzsky-oss <infntyzsky@gmail.com>
2026-01-01 10:09:00 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,18 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE            := reshade
-LOCAL_SRC_FILES         := reshade.cpp
-LOCAL_CPP_EXTENSION     := .cpp
+LOCAL_MODULE := reshade
+LOCAL_SRC_FILES := reshade.cpp
+LOCAL_CPP_EXTENSION := .cpp
 
-# Optimasi + size kecil
-LOCAL_CFLAGS            := -O3 -fvisibility=hidden -ffunction-sections -fdata-sections
-LOCAL_LDFLAGS           := -Wl,--gc-sections
-
-# Library wajib buat hook + GL
-LOCAL_LDLIBS            := -llog -ldl -lEGL -lGLESv2
-
-# Target lama biar kompatibel GTASA
-LOCAL_ARM_MODE          := arm
+LOCAL_CFLAGS += -O3 -fvisibility=hidden -w
+LOCAL_LDLIBS := -llog -lEGL -lGLESv3 -ldl
 
 include $(BUILD_SHARED_LIBRARY)

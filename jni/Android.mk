f431586faec3d6dc5f8d5dc579ea4aa933c83fd9
infntyzsky-oss <infntyzsky@gmail.com>
2026-01-11 15:55:05 +0700

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := reshade
-LOCAL_SRC_FILES := reshade.cpp
-
-LOCAL_LDLIBS := -llog -lEGL -lGLESv3 -ldl
-
-LOCAL_CPPFLAGS := -std=c++17 -fvisibility=hidden
+LOCAL_MODULE := ao_inject
+LOCAL_SRC_FILES := ao_inject.cpp
+LOCAL_LDLIBS := -llog -ldl -landroid
+LOCAL_CFLAGS := -O3 -fvisibility=hidden -Wall -Wextra
+LOCAL_CPPFLAGS := -std=c++14
+LOCAL_ARM_MODE := thumb
 
 include $(BUILD_SHARED_LIBRARY)
